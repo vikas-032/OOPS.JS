13e18654b5177565377ffd73794cc3e898f6de22
vikas-032 <7014.vikas@gmail.com>
2026-01-15 17:38:39 +0530

    Initial commit

---
+++ practice.js
@@ -0,0 +1,165 @@
+// primitive types // value types
+
+//string
+/*
+let x ="ajaysingh";
+console.log(x);
+console.log(typeof(x));
+x = null;
+console.log(typeof(x));
+
+
+// number
+let y =4;
+console.log(y);
+console.log(typeof(y));
+
+
+let z = 1.33;
+console.log(z);
+console.log(typeof(z));
+
+*/
+
+/*
+
+let course = {
+    title:"the project",
+    description:"project11",
+    rating:6
+};
+
+console.log(course);
+console.log(typeof(course));
+
+
+console.log(course.title);
+console.log(course['title']);
+
+*/
+
+// Now we are going to start the topic related to
+// Objects : and the objets have two types one is 
+
+
+// Value types : We understand this by an example like we have physical document and one of our friedn 
+// is take this and make some update but after your update the original document is remain unchanged. So that is callled value types objects
+
+// Reference types : We can understand this by digital method of documentation , if we are sharing our duocument to ourn
+// friend and he make same changes you will see the changes because you both lookig for a same file. So this is called reference type objets.
+
+
+
+
+/*
+let x = "vikas";
+let y= x;
+
+x="something else";
+
+console.log(x);
+console.log(x);
+
+
+let p = {name: "Vikas" };
+let q = p;
+
+p.name ="name  is shivani";
+console.log(p);
+console.log(q);
+
+
+
+
+let j= "my name is khan";
+let n=j;
+
+z= "your name is not khan";
+
+console.log(j);
+console.log(z);
+
+let a = {name:"seema"};
+let b = a;
+
+
+a.name = "and the name is seema";
+console.log(a);
+console.log(b);
+
+*/
+
+
+
+/*
+
+let x = "vikas";
+let y = x;
+
+x=y;
+
+console.log(x);
+
+let p = {name:"shivani"};
+let q= p;
+
+p.name = "my name is vikas not shivani";
+console.log(p);
+
+
+////// We have completed objects topic here
+*/
+
+/*
+
+function add(a , b){
+    return a+b;
+
+}
+
+console.log(add);
+console.log(add(5,  10));
+*/
+/*
+
+function sum(a ,b){
+    return a+b;
+}
+
+function diff(a,b){
+    return a-b;
+}
+
+function operate(operationFunc, a , b){
+    return operationFunc(a, b);
+}
+
+console.log(operate(sum,5,100));
+console.log(operate(diff , 100 ,5));
+*/
+
+
+/*
+function rps(user , computer){
+      if(user === computer) return "draw";
+      if (user ==="secissor" && computer ==="paper") return "computer";
+      if(user ==="rock" && computer ==="secissor") return "user";
+      if (user  === "paper" && computer === "rock") return "user";
+       
+      return "computer";
+
+}
+
+console.log(rps("paper", "paper"));
+
+*/
+
+
+const myNewArray = [500 , 5003 , 564 , 56]
+    
+    function returnThirdValue(getArray){
+    return getArray[2]
+
+    }
+console.log(returnThirdValue(myNewArray));
+
