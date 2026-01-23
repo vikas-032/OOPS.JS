b7a140c283c8cdc755285b9f71b580eea749796c
vikas-032 <7014.vikas@gmail.com>
2026-01-23 17:58:50 +0530

    Update oops.js with inheritance example

--- oops.js
+++ oops.js
@@ -28,11 +28,75 @@ const another = new Circle(1);
 */
 
 
+/*
 class person{
-constructor(){
-    console.log("person created...");
+constructor(n , a){
+    this.name = n;
+    this.age =  a;
+} 
+sayHi(){
+    console.log("hii...")
+}
+static hello(){
+    console.log("hello" );
 }
 }
 
-let person1 = new person();
-console.log(person1);
+class person2{
+    constructor(s , st){
+        this.school = s;
+        this.student = st;
+    }
+
+}
+
+
+let person1 = new person("Vikas", 23);
+let newperson2 = new person2("My own school" , "Shivani");
+
+ 
+console.log(newperson2);
+console.log(person1); 
+person1.sayHi();
+person.hello();
+*/
+
+// Inheritence in OOPS
+
+ class emp{
+    constructor(n){
+        this.name= n;
+    }
+  msg(){
+    console.log("Just a normal txt");
+    
+  }
+ }
+
+  
+ class manager extends emp{
+    constructor(s ,d){
+        super(s);
+this.department = d;
+    }
+msg(){
+    console.log("hlo");
+    
+  }
+    
+    info(){
+        this .msg();
+        console.log(this.name + "is manager of department  " + this.department); 
+    }
+    
+ } 
+
+ class admin extends manager{
+
+ }
+
+  
+
+ let admin1= new admin("shivani  " , "vikas");
+   
+console.log(admin1);
