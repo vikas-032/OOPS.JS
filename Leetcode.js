1a71fed46c913348d4589529b91b680ed4e20a9d
vikas-032 <7014.vikas@gmail.com>
2026-02-25 13:29:24 +0530

    linked list

--- Leetcode.js
+++ Leetcode.js
@@ -1,12 +1,54 @@
 /* Write code that enhances all arrays such that you can call the array.last()
  method on any array and it will return the last element. If there are no
-  elements in the array, it should return -1.
+elements in the array, it should return -1.
 You may assume the array is the output of JSON.parse. */
 
-Array.prototype.last = function() {
+/* Array.prototype.last = function() {
     if(this.length === 0){
         return -1;
     }
     return this[this.length - 1];
 
-}
+} */
+
+
+/* Q2 You are given two non-empty linked lists representing
+ two non-negative integers. The digits are stored in 
+ reverse order, and each of their nodes contains a 
+ single digit. 
+Add the two numbers and return the sum as a linked list.
+ You may assume the two numbers do not contain any leading zero, except the number 0 itself */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next = null) {
+ *   this.val = val;
+ *   this.next = next;
+ * }
+ */
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  let dummy = new ListNode(0);
+  let current = dummy;
+  let carry = 0;
+
+  while (l1 !== null || l2 !== null || carry !== 0) {
+    const val1 = l1 !== null ? l1.val : 0;
+    const val2 = l2 !== null ? l2.val : 0;
+    const sum = val1 + val2 + carry;
+
+    carry = Math.floor(sum / 10);
+    current.next = new ListNode(sum % 10);
+
+    current = current.next;
+    if (l1 !== null) l1 = l1.next;
+    if (l2 !== null) l2 = l2.next;
+  }
+
+  return dummy.next;
+};
