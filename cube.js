c95dd20df43b3c6e52f674e6f1f6287493d92f47
vikas-032 <7014.vikas@gmail.com>
2026-03-25 22:25:35 +0530

    3d cube

---
+++ cube.js
@@ -0,0 +1,13 @@
+const cube = document.querySelector('.cube');
+
+
+
+function rotateCube() {
+
+  cube.style.transform = `rotateX(${Math.random() * 360}deg) rotateY(${Math.random() * 360}deg)`;
+
+}
+
+
+
+setInterval(rotateCube, 3000);
